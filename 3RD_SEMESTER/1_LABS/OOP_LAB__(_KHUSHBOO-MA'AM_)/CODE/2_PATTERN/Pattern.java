66248648428a97963b98611fb4551bcb4ed0cc8e
Mohit Bhati <mohitbhati130306@gmail.com>
2026-01-11 03:18:39 +0530

    Add files via upload

---
+++ 3RD_SEMESTER/1_LABS/OOP_LAB__(_KHUSHBOO-MA'AM_)/CODE/2_PATTERN/Pattern.java
@@ -0,0 +1,15 @@
+class Pattern {
+    public static void main(String args[]) {
+        int i, j;
+        for (i = 1; i <= 4; i++) {
+            for (j = 1; j <= 5; j++) {
+                if (i == 1 || i == 4 || j == 1 || j == 5) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
